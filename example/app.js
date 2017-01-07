f6bd461b4173dabb511043971dfe211ac71adcfd
Raibima Imam Putra <raibima.putra@traveloka.com>
2017-01-08 02:48:35 +0700

    initial commit

---
+++ example/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Coriander from 'coriander';
+
+render(
+  <Coriander />,
+  document.getElementById('app')
+);
---
+++ src/coriander.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Coriander = props => (
+  <div>
+    <h1>Coriander!</h1>
+
+    <style jsx>{`
+      h1 {
+        color: red;
+      }  
+    `}</style>
+  </div>
+);
+
+export default Coriander;
